864010fd67c8a0f92f61c61b7a07d4f6e93212be
Jerrol Krause <jerrolkrause@gmail.com>
2015-10-14 14:20:36 -0700

    Adding first build of application to repo

---
+++ js/controller.js
@@ -0,0 +1,11 @@
+angular.module('gtApp', [])
+        .controller('gtSchedule', function($scope) {
+    $scope.name = 'Moms2 Backyard';
+
+
+
+})
+        .controller('gtCalendar', function($scope) {
+    $scope.name = 'Moms Backyard';
+
+});
---
+++ js/produce.js
@@ -0,0 +1,196 @@
+
+var produce = {
+    broccoli: {
+        label: "Broccoli",
+        maturity: 60,
+        seedling: 4,
+        harvest: 4,
+        spacing: 14,
+        vertical: false,
+        rowsPerBed: 2
+    },
+    broccoli_arcadia: {
+        label: "Broccoli, Arcadia",
+        //maturity: 63,
+        parent: "broccoli"
+    },
+    broccoli_belstar: {
+        label: "Broccoli, Belstar",
+        //maturity: 66,
+        parent: "broccoli"
+    },
+    brussel_sprouts: {
+        label: "Brussel Sprouts",
+        maturity: 90,
+        seedling: 6,
+        harvest: 4,
+        spacing: 18,
+        vertical: false,
+        rowsPerBed: 1
+    },
+    carrot: {
+        label: "Carrots",
+        maturity: 75,
+        seedling: 0,
+        harvest: 16,
+        spacing: 1,
+        vertical: false,
+        rowsPer: 2
+    },
+    cucumber: {
+        label: "Cucumber",
+        maturity: 60,
+        seedling: 3,
+        harvest: 4,
+        spacing: 9,
+        vertical: true,
+        rowsPer: 1
+    },
+    cucumber_diva: {
+        label: "Cucumber, Diva",
+        //maturity: 60,
+        parent: "cucumber"
+    },
+    lettuce: {
+        label: "Lettuce",
+        maturity: 55,
+        seedling: 3,
+        harvest: 2,
+        spacing: 12,
+        vertical: false,
+        rowsPerBed: 2
+    },
+    melon: {
+        label: "Melon",
+        maturity: 85,
+        seedling: 3,
+        harvest: 8,
+        spacing: 9,
+        vertical: true,
+        rowsPerBed: 1
+    },
+    melon_earlichamp: {
+        label: "Melon, Earlichamp Cantalope",
+        //maturity: 72,
+        parent: "melon"
+    },
+    melon_diplomat: {
+        label: "Melon, Diplomat Galia",
+        //maturity: 70,
+        parent: "melon"
+    },
+    melon_serenade: {
+        label: "Melon, Serenade Butterscotch",
+        //maturity: 80,
+        parent: "melon"
+    },
+    melon_sapomiel: {
+        label: "Melon, Sapomiel Piel Del Sapo",
+        //maturity: 80,
+        parent: "melon"
+    },
+    potato: {
+        label: "Potatos",
+        maturity: 65,
+        seedling: 2,
+        harvest: 8,
+        spacing: 8,
+        vertical: false,
+        rowsPerBed: 2
+    },
+    potato_yukon_gold: {
+        label: "Potato, Yukon Gold",
+        maturity: 80,
+        parent: "potato"
+    },
+    potato_kennebec: {
+        label: "Potato, Kennebec",
+        maturity: 100,
+        parent: "potato"
+    },
+    pumpkin: {
+        label: "Pumpkin",
+        maturity: 100,
+        seedling: 3,
+        harvest: 4,
+        spacing: 24,
+        vertical: false,
+        rowsPer: 1
+    },
+    sweet_potato: {
+        label: "Sweet Potato",
+        maturity: 120,
+        seedling: 8,
+        harvest: 1,
+        spacing: 12,
+        vertical: false,
+        rowsPerBed: 2
+    },
+    squash: {
+        label: "Squash",
+        maturity: 90,
+        seedling: 3,
+        harvest: 14,
+        spacing: 9,
+        vertical: true,
+        rowsPer: 1
+    },
+    squash_butternut: {
+        label: "Squash, Waltham Butternut",
+        //maturity: 105,
+        parent: "squash"
+    },
+    squash_butternut_butterscotch: {
+        label: "Squash, Butterscotch PMR",
+        //maturity: 100,
+        parent: "squash"
+    },
+    squash_acorn_honeybear: {
+        label: "Squash, Honey Bear",
+        //maturity: 85,
+        parent: "squash"
+    },
+    swiss_chard: {
+        label: "Swiss Chard",
+        maturity: 50,
+        seedling: 4,
+        harvest: 20,
+        spacing: 6,
+        vertical: false,
+        rowsPer: 2
+    },
+    tomato: {
+        label: "Tomatos",
+        maturity: 90,
+        seedling: 8,
+        harvest: 16,
+        spacing: 9,
+        vertical: true,
+        rowsPer: 1
+    },
+    tomato_big_beef: {
+        label: "Tomato, Big Beef",
+        //maturity: 70,
+        parent: "tomato"
+    },
+    tomato_sun_gold: {
+        label: "Tomato, Sun Gold",
+        //maturity: 57,
+        parent: "tomato"
+    },
+    tomato_black_cherry: {
+        label: "Tomato, Black Cherry",
+        //maturity: 64,
+        parent: "tomato"
+    },
+    tomato_matts_wild_cherry: {
+        label: "Tomato, Matts Wild Cherry",
+        //maturity: 60,
+        parent: "tomato"
+    },
+    tomato_supersweet_100: {
+        label: "Tomato, Supersweet 100",
+        //maturity: 60,
+        parent: "tomato"
+    }
+};
